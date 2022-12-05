d41050d24605c57b45464bf992c2f225871cca51
Flavio Mota <flaviomoc@gmail.com>
2022-12-05 07:33:05 -0300

    function updated
    now it uses terra::app to calculates alpha diversity

--- R/alphadv.R
+++ R/alphadv.R
@@ -1,4 +1,24 @@
-#' Alpha diversity calculation
+#' Alpha diversity calculation for vector
+#'
+#' @param bin Object of class SpatRaster with binarized distribution projected to all species from climate scenario 1
+#' @return Object of class SpatRaster with alpha diversity/species richness
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' set.seed(100)
+#' ref <- terra::rast(array(sample(c(rep(1, 750), rep(0, 250))), dim = c(20, 20, 10)))
+#' names(ref) <- paste0("sp", 1:10)
+#' ref
+#' ref.rich <- alphadv(ref)
+#' ref.rich
+#' }
+.alphadv <- function(bin){
+  r <- sum(bin > 0)
+  return(r)
+}
+
+#' Alpha diversity calculation for raster
 #'
 #' @param bin1 Object of class SpatRaster with binarized distribution projected to all species from climate scenario 1
 #' @param bin2 Object of class SpatRaster with binarized distribution projected to all species from climate scenario 2
@@ -7,6 +27,7 @@
 #' @export
 #'
 #' @examples
+#' \dontrun{
 #' set.seed(100)
 #' ref <- terra::rast(array(sample(c(rep(1, 750), rep(0, 250))), dim = c(20, 20, 10)))
 #' names(ref) <- paste0("sp", 1:10)
@@ -20,15 +41,16 @@
 #' fut.rich
 #' delta.rich <- alphadv(ref, fut)
 #' delta.rich
+#' }
 alphadv <- function(bin1, bin2){
-  if(missing(bin2) == "TRUE"){
-    r <- sum(bin1 > 0) # species richness
-    names(r) <- "Richness"
-  } else {
-    r2 <- sum(bin2 > 0)
-    r1 <- sum(bin1 > 0)
+  if(!missing(bin2)){
+    r1 <- terra::app(bin1, .alphadv)
+    r2 <- terra::app(bin2, .alphadv)
     r <- r2 - r1 # delta richness
     names(r) <- "Delta richness"
+  } else {
+    r <- terra::app(bin1, .alphadv) # species richness
+    names(r) <- "Richness"
   }
   return(r)
 }
