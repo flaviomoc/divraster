cd46353d1a8ebca948a01fcf7afada4dec49c558
Flávio Mota <flaviomoc@gmail.com>
2022-12-17 09:40:40 -0300

    function alphadv updated
    vectorized
    
    tests updated
    
    vignettes updated

--- R/alphadv.R
+++ R/alphadv.R
@@ -1,9 +1,8 @@
 #' Alpha diversity calculation for raster
 #'
-#' @param bin1 Object of class SpatRaster with binarized distribution projected to all species from climate scenario 1
-#' @param bin2 Object of class SpatRaster with binarized distribution projected to all species from climate scenario 2
+#' @param bin Object of class SpatRaster with binarized distribution projected to all species for a given climate scenario
 #'
-#' @return Object of class SpatRaster with alpha diversity/species richness. If bin2 is provide, it calculates delta richness
+#' @return Object of class SpatRaster with alpha diversity/species richness
 #' @export
 #'
 #' @examples
@@ -12,25 +11,11 @@
 #' ref <- terra::rast(array(sample(c(rep(1, 750), rep(0, 250))), dim = c(20, 20, 10)))
 #' names(ref) <- paste0("sp", 1:10)
 #' ref
-#' fut <- terra::rast(array(sample(c(rep(1, 300), rep(0, 700))), dim = c(20, 20, 10)))
-#' names(fut) <- paste0("sp", 1:10)
-#' fut
 #' ref.rich <- alphadv(ref)
 #' ref.rich
-#' fut.rich <- alphadv(fut)
-#' fut.rich
-#' delta.rich <- alphadv(ref, fut)
-#' delta.rich
 #' }
-alphadv <- function(bin1, bin2){
-  if(!missing(bin2)){
-    r1 <- terra::app(bin1, sum, na.rm = TRUE)
-    r2 <- terra::app(bin2, sum, na.rm = TRUE)
-    r <- r2 - r1 # delta richness
-    names(r) <- "Delta richness"
-  } else {
-    r <- terra::app(bin1, sum, na.rm = TRUE) # species richness
-    names(r) <- "Richness"
-  }
-  return(c(r, r)) # terra::app function does not work when it returns only one raster
+alphadv <- function(bin){
+  r <- terra::app(bin, sum, na.rm = TRUE) # species richness
+  names(r) <- "Richness"
+  return(r) # terra::app function does not work when it returns only one raster
 }
--- tests/testthat/test-alphadv.R
+++ tests/testthat/test-alphadv.R
@@ -7,10 +7,8 @@ test_that("function alphadv works", {
   # applying the function
   r.rich <- alphadv(ref)
   f.rich <- alphadv(fut)
-  delta <- alphadv(ref, fut)
 
   # testing
-  expect_equal(r.rich@ptr$range_min[1], 12)
-  expect_equal(f.rich@ptr$range_min[1], 5)
-  expect_equal(delta@ptr$range_min[1], -23)
+  expect_equal(r.rich@ptr$range_min, 12)
+  expect_equal(f.rich@ptr$range_min, 5)
 })
