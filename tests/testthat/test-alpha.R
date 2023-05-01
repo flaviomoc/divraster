af76b74e46b725cb44ca79dc67b2a386b6e36b15
Flávio Mota <flaviomoc@gmail.com>
2023-05-01 11:06:20 -0300

    minor changes in tests and vignette

--- tests/testthat/test-alpha.R
+++ tests/testthat/test-alpha.R
@@ -2,7 +2,6 @@ test_that("function spat.alpha works", {
 
   # loading data
   bin1 <- terra::rast(system.file("extdata", "ref.tif", package = "DMSD"))
-  bin2 <- terra::rast(system.file("extdata", "fut.tif", package = "DMSD"))
   traits <- read.csv(system.file("extdata", "traits.csv", package = "DMSD"), row.names = 1)
   tree <- ape::read.tree(system.file("extdata", "tree.tre", package = "DMSD"))
 
@@ -12,6 +11,9 @@ test_that("function spat.alpha works", {
   alpha.pd <- spat.alpha(bin1, tree)
 
   # testing
+  expect_true(class(bin1) == "SpatRaster", "TRUE")
+  expect_true(class(tree) == "phylo", "TRUE")
+  expect_equal(dim(traits), c(10, 2))
   expect_equal(alpha.td@ptr$range_min, 2)
   expect_equal(round(alpha.fd@ptr$range_min, 2), .24)
   expect_equal(round(alpha.pd@ptr$range_min, 2), 3.1)
--- tests/testthat/test-beta_spatial.R
+++ tests/testthat/test-beta_spatial.R
@@ -2,7 +2,6 @@ test_that("function spat.beta works", {
 
   # loading data
   bin1 <- terra::rast(system.file("extdata", "ref.tif", package = "DMSD"))
-  bin2 <- terra::rast(system.file("extdata", "fut.tif", package = "DMSD"))
   traits <- read.csv(system.file("extdata", "traits.csv", package = "DMSD"), row.names = 1)
   tree <- ape::read.tree(system.file("extdata", "tree.tre", package = "DMSD"))
 
