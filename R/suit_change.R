7aab8de6226e03df9f05cd9137321856ae57b893
flaviomoc <flaviomoc@gmail.com>
2024-03-19 08:47:25 -0600

    new function added
    it calculates the change in climate suitable area between scenarios by species

---
+++ R/suit_change.R
@@ -0,0 +1,33 @@
+#' Species suitability change between climate scenarios
+#'
+#' @param r1 A SpatRaster with presence-absence data (0 or 1)
+#' for a set of species.
+#' @param r2 A SpatRaster with presence-absence data (0 or 1)
+#' for a set of species.
+#' @param filename Character. Save results if a name is provided.
+#'
+#' @return A SpatRaster with suitability change (gain, loss,
+#' unchanged, and unsuitable).
+#' @export
+#'
+#' @examples
+#' #' \donttest{
+#' library(terra)
+#' r1 <- terra::rast(system.file("extdata", "ref.tif",
+#' package = "divraster"))
+#' r2 <- terra::rast(system.file("extdata", "fut.tif",
+#' package = "divraster"))
+#' suit.change(r1[[1]], r2[[1]])
+#' }
+suit.change <- function(r1, r2, filename = "") {
+  gain <- (r2 == 1) & (r1 == 0)
+  loss <- (r2 == 0) & (r1 == 1)
+  no_change <- (r2 == 1) & (r1 == 1)
+  unsuitable_both <- (r2 == 0) & (r1 == 0)
+  change_map <- gain * 1 + loss * 2 + no_change * 3 + unsuitable_both * 4
+  if (filename != "") {
+    terra::writeRaster(change_map, filename = filename, overwrite = TRUE)
+  }
+
+  return(change_map)
+}
